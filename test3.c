fe6b33a138cb65d01641c34779d2cc9de5ca1211
komura yukihiro <comcom022@hotmail.co.jp>
2012-11-22 09:29:03 +0900

    最初のコミット

---
+++ test3.c
@@ -0,0 +1,22 @@
+
+#define nx 64
+
+#include <stdio.h> 
+#include <omp.h> 
+int main(void) 
+{ 
+  int i,j, a[nx], new_nx; 
+    j = omp_get_num_procs();  
+
+  new_nx = ((int)(nx/j)+1)*j;
+  #pragma omp parallel for ordered num_threads(j)
+  for(i=0;i<new_nx;i++){ 
+   if(i<nx){
+    a[i]=0; 
+    #pragma omp ordered 
+    printf(" i=%d thread_num=%d\n",i,omp_get_thread_num()); 
+    }
+   } 
+
+
+} 
